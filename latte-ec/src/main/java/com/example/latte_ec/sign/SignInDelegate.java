794897aaa5e183415d53b7fac2a24ffc0a04a5f6
Administrator <https://github.com/heihie123/turbo-octo-enigma.git>
2018-10-23 18:29:27 +0800

    登录页

--- latte-ec/src/main/java/com/example/latte_ec/sign/SignInDelegate.java
+++ latte-ec/src/main/java/com/example/latte_ec/sign/SignInDelegate.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatTextView;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,23 +14,20 @@ import com.example.latte_core.ui.loader.LatteLoader;
 import com.example.latte_ec.R;
 
 /**
- * 注册
+ * 登录
  */
-public class SignUpDelegate extends LatteDelegate implements View.OnClickListener {
+public class SignInDelegate extends LatteDelegate implements View.OnClickListener {
 
-    private TextInputEditText mNameEdit;
-    private TextInputEditText mEmailEdit;
     private TextInputEditText mPhoneEdit;
     private TextInputEditText mPwdEdit;
-    private TextInputEditText mConfirmPwdEdit;
-    private AppCompatButton mSignUpBen;
-    private AppCompatTextView mLinkSignInTxt;
+    private AppCompatButton mSignInBen;
+    private AppCompatTextView mLinkSignUpTxt;
 
     Handler handler = new Handler();
 
     @Override
     public Object setLayout() {
-        return R.layout.delegate_sign_up;
+        return R.layout.delegate_sign_in;
     }
 
     @Override
@@ -41,39 +37,36 @@ public class SignUpDelegate extends LatteDelegate implements View.OnClickListene
     }
 
     private void initView() {
-        mNameEdit = $(R.id.edit_sign_up_name);
-        mEmailEdit = $(R.id.edit_sign_up_email);
-        mPhoneEdit = $(R.id.edit_sign_up_phone);
-        mPwdEdit = $(R.id.edit_sign_up_pwd);
-        mConfirmPwdEdit = $(R.id.edit_sign_up_confirm_pwd);
-        mSignUpBen = $(R.id.btn_sign_up);
-        mLinkSignInTxt = $(R.id.txt_link_sign_in);
+        mPhoneEdit = $(R.id.edit_sign_in_phone);
+        mPwdEdit = $(R.id.edit_sign_in_pwd);
+        mSignInBen = $(R.id.btn_sign_in);
+        mLinkSignUpTxt = $(R.id.txt_link_sign_up);
     }
 
     private void initEvent() {
-        mSignUpBen.setOnClickListener(this);
-        mLinkSignInTxt.setOnClickListener(this);
+        mSignInBen.setOnClickListener(this);
+        mLinkSignUpTxt.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         int i = view.getId();
-        if (i == R.id.btn_sign_up) {
-            onClickSignUp();
+        if (i == R.id.btn_sign_in) {
+            onClickSignIn();
         }
 
-        if (i == R.id.txt_link_sign_in) {
+        if (i == R.id.txt_link_sign_up) {
             onClickLink();
         }
     }
 
-    private void onClickSignUp() {
+    private void onClickSignIn() {
         LatteLoader.showLoading(mContext);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 if (checkForm()) {
-                    Toast.makeText(mContext, "注册成功", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(mContext, "登录成功", Toast.LENGTH_SHORT).show();
                 }
                 LatteLoader.stopLoading();
             }
@@ -81,25 +74,10 @@ public class SignUpDelegate extends LatteDelegate implements View.OnClickListene
     }
 
     private boolean checkForm() {
-        String nameStr = mNameEdit.getText().toString();
-        String emailStr = mEmailEdit.getText().toString();
         String phoneStr = mPhoneEdit.getText().toString();
         String pwdStr = mPwdEdit.getText().toString();
-        String confirmPwdStr = mConfirmPwdEdit.getText().toString();
         boolean isPass = true;
 
-        if (nameStr.isEmpty()) {
-            mNameEdit.setError("请输入姓名");
-            isPass = false;
-        } else {
-            mNameEdit.setError(null);
-        }
-        if (emailStr.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(emailStr).matches()) {
-            mEmailEdit.setError("邮箱格式错误");
-            isPass = false;
-        } else {
-            mEmailEdit.setError(null);
-        }
         if (phoneStr.isEmpty() || phoneStr.length() != 11) {
             mPhoneEdit.setError("手机号错误");
             isPass = false;
@@ -112,16 +90,10 @@ public class SignUpDelegate extends LatteDelegate implements View.OnClickListene
         } else {
             mPwdEdit.setError(null);
         }
-        if (confirmPwdStr.isEmpty() || confirmPwdStr.length() < 6 || !pwdStr.equals(confirmPwdStr)) {
-            mConfirmPwdEdit.setError("密码验证错误");
-            isPass = false;
-        } else {
-            mConfirmPwdEdit.setError(null);
-        }
         return isPass;
     }
 
     private void onClickLink() {
-        getSupportDelegate().start(new SignInDelegate());
+        getSupportDelegate().start(new SignUpDelegate());
     }
 }
