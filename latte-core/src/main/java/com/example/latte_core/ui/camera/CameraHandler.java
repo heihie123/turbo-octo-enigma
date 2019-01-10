2d1a6751a3d0baf02c4303a2dece6401268c60b7
Administrator <https://github.com/heihie123/turbo-octo-enigma.git>
2019-01-10 18:32:29 +0800

    11

--- latte-core/src/main/java/com/example/latte_core/ui/camera/CameraHandler.java
+++ latte-core/src/main/java/com/example/latte_core/ui/camera/CameraHandler.java
@@ -1,4 +1,56 @@
 package com.example.latte_core.ui.camera;
 
-public class CameraHandler {
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+
+import com.example.latte_core.R;
+import com.example.latte_core.detegates.PermissionCheckDelegate;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class CameraHandler implements View.OnClickListener {
+
+    private final AlertDialog DIALOG;
+    private final PermissionCheckDelegate DELEGATE;
+
+    public CameraHandler(PermissionCheckDelegate delegate) {
+        this.DELEGATE = delegate;
+        this.DIALOG = new AlertDialog.Builder(delegate.getContext()).create();
+    }
+
+    final public void beginCameraDialog() {
+        DIALOG.show();
+        final Window window = DIALOG.getWindow();
+        if (window != null) {
+            window.setContentView(R.layout.dialog_camera_panel);
+            window.setGravity(Gravity.BOTTOM);
+            window.setWindowAnimations(R.style.anim_panel_up_from_bottom);
+            window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_cancel) {
+            DIALOG.cancel();
+        } else if (id == R.id.btn_photo) {
+            takePhoto();
+            DIALOG.cancel();
+        } else if (id == R.id.btn_native) {
+            pickPhoto();
+            DIALOG.cancel();
+        }
+    }
+
+    private void takePhoto() {
+
+    }
+
+    private void pickPhoto() {
+
+    }
 }
